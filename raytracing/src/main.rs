f824dc378cc56f537b792f51b95a91ed5374f8ea
Saulo Ramos <srdcp@hotmail.com>
2026-06-15 16:39:33 -0600

    Add sphere hit detection and normal shading

--- raytracing/src/main.rs
+++ raytracing/src/main.rs
@@ -29,7 +29,7 @@ fn main() -> io::Result<()> {
 
     // Calculate the vectors across the horizontal and down the vertical viewport edges.
     let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
-    let viewport_v = Vec3::new(0.0, viewport_height, 0.0);
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
 
     // Calculate the horizontal and vertical delta vectors from pixel to pixel.
     let pixel_delta_u = viewport_u / image_width as f32;
@@ -65,19 +65,25 @@ fn main() -> io::Result<()> {
 }
 
 fn ray_color(r: Ray) -> Color {
-    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
-        return Color::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let n = Vec3::unit_vector(r.at(t) - Vec3::new(0.0, 0.0, -1.0));
+        return 0.5 * Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
     }
     let unit_direction = Vec3::unit_vector(r.direction());
     let a = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0);
 }
 
-fn hit_sphere(center: Point3, radius: f32, r: Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f32, r: Ray) -> f32 {
     let oc: Vec3 = center - r.origin();
     let a = Vec3::dot(r.direction(), r.direction());
     let b = -2.0 * Vec3::dot(r.direction(), oc);
     let c = Vec3::dot(oc, oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    return discriminant as i32 >= 0;
+
+    if discriminant < 0.0 {
+        return -1.0;
+    }
+    return (-b - discriminant.sqrt()) / (2.0 * a);
 }
